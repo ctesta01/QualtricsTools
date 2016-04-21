4cf698c447fd1617342bec7670a1fa6796895914
Christian Testa <testa101@gmail.com>
2016-04-21 16:34:06 -0400

    user submitted surveys validated for no duplicate data export tags in shinyServer

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -1,14 +1,19 @@
 library(shiny)
 library(knitr)
+library(rjson)
 
 shinyServer(
   function(input, output) {
 
 
    responses <- reactive({ load_csv_data(input$file1) })
-   survey <- reactive({ load_qsf_data(input$file2) })
+   survey <- reactive({
+    validate(
+      need(validate_data_export_tags(questions_from_survey(load_qsf_data(input$file2))),
+           "Please submit a survey with no duplicate question IDs"))
+    load_qsf_data(input$file2) })
 
-    output$contents <- renderText({ sapply(blocks_from_survey(survey()), function(x) x$Description) })
+    output$contents <- renderText({ sapply(questions_from_survey(survey()), function(x) x$Payload$QuestionText) })
 
   }
 )
