bcc812373d0bca70737e52272a4f4585071b189d
Christian Testa <testa101@gmail.com>
2016-05-02 16:14:20 -0400

    Renamed several functions to be clearer, and completed the mc_single_answer_results function

--- R/question_type_checking.R
+++ R/question_type_checking.R
@@ -1,23 +1,21 @@
-#' Determine if a question is a multiple choice question
+#' Determine if a question is a multiple choice and multiple answer question
 #'
 #' Each of the is-functions defined in the qualtrics package are used
-#' for determining which response parsing function should be used. A 
-#' function is considered multiple choice if it is listed in 
-#' the qsf file as having $Payload$QuestionType == "MC" 
-#' (standing for Multiple Choice), and the `$Payload$Selector` is set to one of the 
+#' for determining which response parsing function should be used. A
+#' function is considered multiple choice if it is listed in
+#' the qsf file as having $Payload$QuestionType == "MC"
+#' (standing for Multiple Choice), and the `$Payload$Selector` is set to one of the
 #' following:
-#' "Multiple Answer Vertical", 
+#' "Multiple Answer Vertical",
 #' "Multiple Answer Horizontal",
 #' "Multiple Choice Select Box",
-#' "Multiple Answer Column", 
-#' or if it is a `$Payload$QuestionType == "Matrix"` question with 
-#' `$Payload$SubSelector == "MultipleAnswer"`
+#' "Multiple Answer Column",
 #'
 #' @param question The question parameter is a single question from a qualtrics survey.
 #'
 #' @return The return value of this is a boolean, true if it is one of these kinds of
-#' questions and false otherwise. 
-is_multiple_choice <- function(question) {
+#' questions and false otherwise.
+is_mc_multiple_answer <- function(question) {
 
     is_Multiple_Choice = (question$Payload$QuestionType == "MC")
     has_MultipleAnswer_selector = (question$Payload$Selector == "MAVR" ||
@@ -25,37 +23,48 @@ is_multiple_choice <- function(question) {
                                    question$Payload$Selector == "MSB" ||
                                    question$Payload$Selector == "MACOL")
 
-    is_Matrix = (question$Payload$QuestionType == "Matrix")
-    has_Matrix_MA_selector = (question$Payload$SubSelector == "MultipleAnswer")
+    is_Multiple_Answer = (is_Multiple_Choice && has_MultipleAnswer_selector)
 
-    is_Multiple_Answer = (is_Multiple_Choice && has_MultipleAnswer_selector) ||
-    (is_Matrix && has_Matrix_MA_selector)
-
-    return(is_Multiple_Answer)
+    return(is_Multiple_Choice && has_MultipleAnswer_selector)
 }
+
+
+#' Determine if a question is a matrix and multiple answer question
+#'
+#' A question is considered a matrix multiple answer question if it
+#' is a `$Payload$QuestionType == "Matrix"` question with
+#' `$Payload$SubSelector == "MultipleAnswer"`
+#'
+#' @param question The question parameter is a single question from a qualtrics survey.
+#' @return The return value of this is a boolean, true if it is one of these kinds of
+#' questions and false otherwise.
+is_matrix_multiple_answer <- function(question) {
+  is_Matrix = (question$Payload$QuestionType == "Matrix")
+  has_Matrix_MA_selector = (question$Payload$SubSelector == "MultipleAnswer")
+
+  return(is_Matrix && has_Matrix_MA_selector)
+}
+
 
 #' Determine if a question is a single answer question
 #'
 #' Each of the is-functions defined in the qualtrics package are used
-#' for determining which response parsing function should be used. A 
-#' function is considered multiple choice if it is listed in 
-#' the qsf file as having $Payload$QuestionType == "MC" 
-#' (standing for Multiple Choice), and the `$Payload$Selector` is set to one of the 
+#' for determining which response parsing function should be used. A
+#' function is considered multiple choice if it is listed in
+#' the qsf file as having $Payload$QuestionType == "MC"
+#' (standing for Multiple Choice), and the `$Payload$Selector` is set to one of the
 #' following:
-#' "Single Answer Vertical", 
+#' "Single Answer Vertical",
 #' "Single Answer Horizontal",
 #' "Single Answer Column",
 #' "Dropdown List",
 #' "Select Box",
-#' or if it is a `$Payload$QuestionType == "Matrix"` question with 
-#' `$Payload$SubSelector == "MultipleAnswer"`
 #'
 #' @param question The question parameter is a single question from a qualtrics survey.
 #'
 #' @return The return value of this is a boolean, true if it is one of these kinds of
-#' questions and false otherwise. 
-
-is_single_answer <- function(question) {
+#' questions and false otherwise.
+is_mc_single_answer <- function(question) {
     is_Multiple_Choice = (question$Payload$QuestionType == "MC")
     has_SingleAnswer_selector = (question$Payload$Selector == "SAVR" ||
                                  question$Payload$Selector == "SAHR" ||
@@ -70,39 +79,56 @@ is_single_answer <- function(question) {
                               question$Payload$SubSelector == "SingleAnswer")
 
 
-    is_Single_Answer = (is_Multiple_Choice && has_SingleAnswer_selector) || 
-    (is_Matrix && has_Matrix_SA_selector)
-
-    return(is_Single_Answer)
+    return(is_Multiple_Choice && has_SingleAnswer_selector)
 }
+
+#' Determine if a question is a matrix and multiple answer question
+#'
+#' A question is considered a matrix multiple answer question if it
+#' is a `$Payload$QuestionType == "Matrix"` question with
+#' `$Payload$SubSelector == "MultipleAnswer"`
+#'
+#' @param question The question parameter is a single question from a qualtrics survey.
+#'
+#' @return The return value of this is a boolean, true if it is one of these kinds of
+#' questions and false otherwise.
+is_matrix_single_answer <- function(question) {
+  is_Matrix = question$Payload$QuestionType == "Matrix"
+
+  has_Matrix_SA_selector = (question$Payload$SubSelector == "DL" ||
+                              question$Payload$Selector == "Bipolar" ||
+                              question$Payload$SubSelector == "SingleAnswer")
+
+  return(is_Matrix && has_Matrix_SA_selector)
+}
+
 
 #' Determine if a question is a rank order question
 #'
-#' @inheritParam is_multiple_choice
-#'
+#' @inheritParam is_mc_multiple_choice
 #' @return The return value of this is a boolean, true if it is one of these kinds of
-#' questions and false otherwise. 
+#' questions and false otherwise.
 is_rank_order <- function(x) {
   return(x$Payload$QuestionType == "RO")
 }
 
-#' Determine if a question is a text order question
-#'
-#' @inheritParam is_multiple_choice
-#'
-#' @return The return value of this is a boolean, true if it is one of these kinds of
-#' questions and false otherwise. 
 
+#' Determine if a question is a text entry question
+#'
+#' @inheritParams is_mc_multiple_choice
+#' @return The return value of this is a boolean, true if it is one of these kinds of
+#' questions and false otherwise.
 is_text_entry <- function(x) {
   return(x$Payload$QuestionType == "TE")
 }
+
 
 #' Determine if a question is a Matrix question
 #'
 #' @inheritParam is_multiple_choice
 #'
 #' @return The return value of this is a boolean, true if it is one of these kinds of
-#' questions and false otherwise. 
+#' questions and false otherwise.
 is_matrix_question <- function(x) {
   return(x$Payload$QuestionType == "Matrix")
 }
--- R/results_generation.R
+++ R/results_generation.R
@@ -1,37 +1,64 @@
 #' Format Percents
 #'
-#' percent0 is a function for formatting a decimal number into a string representing a percentage with 
-#' a specific number of decimal places and with a "%" appended. 
-#' The function uses formatC from the R base package and 
-#' defaults to formatting the number with 1 digit and with the "f" format 
-#' in the formatC argument. 
+#' percent0 is a function for formatting a decimal number into a string representing a percentage with
+#' a specific number of decimal places and with a "%" appended.
+#' The function uses formatC from the R base package and
+#' defaults to formatting the number with 1 digit and with the "f" format
+#' in the formatC argument.
 
 percent0 <- function(x, digits = 1, format = "f", ...) {
   paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
 }
 
 
-
+#' Create the Results Table for a Multiple Choice Single Answer Question
+#'
+#' The mc_single_answer_results function uses the definition of the choices in the QSF file
+#' and their potentially recoded values to determine how to table the results paired to that question
+#'
+#'
+#' @param question The question must have a paired response column placed into the question
+#' under `$Responses`
+#' @return a table with an N, Percent, and choice column, detailing the number of responses for each
+#' choice.
 mc_single_answer_results <- function(question) {
-
+    # the factors are the variable codes that users could choose between
+    # if a question has been recoded, the variable names are in $Payload$RecodeValues
+    # and if not, they are in $Payload$Choices
     if ("RecodeValues" %in% names(question$Payload)) {
         factors <- unlist(question$Payload$RecodeValues)
     } else {
         factors <- names(questions[[3]]$Payload$Choices)
     }
 
+    # the responses to single answer questions are only one column, so they are always the first column.
+    # take the responses and sort them by the factors to get responses_tabled.
+    # the second column of the responses_tabled are the numbers of responses for each factor, our Ns.
+    # total number of responses to a question is counted by all the answers that aren't -99
+    # use Ns and respondent_count to calculate the percents for each factor.
     responses <- question$Responses[[1]]
     responses_tabled <- as.data.frame(table(factor(responses, levels=factors)))
-    Ns <- responses_tabled[,2]
+    N <- responses_tabled[,2]
+    respondent_count <- length(question$Responses[[1]] != -99)
+    Percent <- percent0(N / respondent_count)
 
-
+    # if the choice variables have been recoded, first the factors are retrieved from the responses_tabled,
+    # then they are turned into the list of corresponding indexes in the RecodeValues list,
+    # which are then used to recover the original choice text from the Choices list,
+    # and then the choices are flattened to a cleaner list.
+    # if the choice variables are not recoded, then they can be retrieved directly from the responses_table
     if ("RecodeValues" %in% names(question$Payload)) {
         choices_recoded <- responses_tabled[,1]
-        choices_uncoded <- sapply(Choices, function(x) which(questions[[3]]$Payload$RecodeValues == x))
-        choices <- sapply(Choices, function(x) question$Payload$Choices[[x]])
+        choices_uncoded <- sapply(choices_recoded, function(x) which(question$Payload$RecodeValues == x))
+        choices <- sapply(choices_uncoded, function(x) question$Payload$Choices[[x]])
+        choices <- unlist(choices, use.names = FALSE)
     } else {
-        choices <- responses_tabled[,2]
+        choices <- responses_tabled[,1]
     }
 
-
+    # construct the results table with a column for N, Percent, and choices,
+    # but make sure that the choices column doesn't have a header when it prints.
+    results_table <- data.frame(N, Percent, choices)
+    colnames(results_table)[3] <- ""
+    results_table
 }
