2b2d2dae96f32f268d7eeb3f6c3d65fa633fea2e
Christian Testa <testa101@gmail.com>
2017-06-06 13:39:18 -0400

    Adding new tests, and updating others, such that individual questions are processed rather than entire surveys. The tests will run faster this way, and it also prevents me from having to compare each individual manually.

--- tests/testthat/test_mcsa_question_processing.R
+++ tests/testthat/test_mcsa_question_processing.R
@@ -1,35 +1,95 @@
 # Test mc_single_answer_results
 
 context("Testing mc_single_answer_results")
-rm(list=ls()) # clear the environment
+rm(list = ls()) # clear the environment
 # surveysdir is the sample surveys filepath
 surveysdir = file.path(path.package('QualtricsTools'), 'data/Sample Surveys/')
 
-test_that("Test that mc_single_answer_results is correct for q2_favorite in the Better Sample Survey", {
-  survey_dir = file.path(surveysdir, "Better Sample Survey")
-  qsf = file.path(survey_dir, "Better_Sample_Survey.qsf")
-  csv = file.path(survey_dir, "Better_Sample_Survey.csv")
-  # Use capture.output to suppress printed messages in testing.
-  # Syntax from https://stackoverflow.com/questions/2723034/suppress-one-commands-output-in-r
-  capture.output(get_setup(headerrows=3, qsf_path=qsf, csv_path=csv), file='NUL')
-  # the only things which should be in the environment are now
-  # survey, responses, flow, blocks, questions, original_first_rows,
-  # surveysdir, and survey_dir.
+# Better Sample Survey: q2_favorite
+test_that(
+  "Test that mc_single_answer_results is correct for q2_favorite in the Better Sample Survey",
+  {
+    original_first_rows <- readRDS(
+      file.path(
+        find.package('QualtricsTools'),
+        'data/Sample Surveys/Better Sample Survey/RDS',
+        'original_first_rows.rds'
+      )
+    )
+    q2_favorite <- readRDS(
+      file.path(
+        find.package('QualtricsTools'),
+        'data/Sample Surveys/Better Sample Survey/RDS',
+        'q2_favorite.rds'
+      )
+    )
+    q2_favorite <-
+      process_question_results(q2_favorite, original_first_rows)
+    q2_favorite_results_table <- readRDS(
+      file.path(
+        find.package('QualtricsTools'),
+        'data/Sample Surveys/Better Sample Survey/RDS',
+        'q2_favorite_results_table.rds'
+      )
+    )
+    expect_identical(q2_favorite[['Table']], q2_favorite_results_table)
+  }
+)
 
-  index1 = find_question_index(questions, 'q2_favorite')
-  df = as.data.frame(questions[[index1]][['Table']])
-  expect_equal(df[['N']], c(4,5,1))
-  expect_equal(as.character(df[["Percent"]]), c('40.0%', '50.0%', '10.0%'))
-  expect_equal(as.character(df[[3]]), c('Canteloupe', 'Kiwi', 'Pomagranite'))
-})
+# Long Exhaustive Sample Survey: Q2
+test_that(
+  "Test that mc_single_answer_results is correct for Q2 in the Long Exhaustive Sample Survey",
+  {
+    original_first_rows <- readRDS(
+      file.path(
+        find.package('QualtricsTools'),
+        'data/Sample Surveys/Long Exhaustive Sample Survey/RDS',
+        'original_first_rows.rds'
+      )
+    )
+    Q2 <- readRDS(
+      file.path(
+        find.package('QualtricsTools'),
+        'data/Sample Surveys/Long Exhaustive Sample Survey/RDS',
+        'Q2.rds'
+      )
+    )
+    Q2 <- process_question_results(Q2, original_first_rows)
+    Q2_results_table <-  readRDS(
+      file.path(
+        find.package('QualtricsTools'),
+        'data/Sample Surveys/Long Exhaustive Sample Survey/RDS',
+        'Q2_results_table.rds'
+      )
+    )
+    expect_identical(Q2[['Table']], Q2_results_table)
+  }
+)
 
-test_that("Test that mc_single_answer_results is correct for ", {
-  survey_dir = file.path(surveysdir, "Long Exhaustive Sample Survey")
-  qsf = file.path(survey_dir, "Long_Exhaustive_Sample_Survey.qsf")
-  csv = file.path(survey_dir, "Long_Exhaustive_Sample_Survey.csv")
-  # Use capture.output to suppress printed messages from get_setup in testing.
-  # Syntax from https://stackoverflow.com/questions/2723034/suppress-one-commands-output-in-r
-  capture.output(get_setup(headerrows=3, qsf_path=qsf, csv_path=csv), file='NUL')
+test_that(
+  "Test that mc_single_answer_results is correct for Q10 in the Long Exhaustive Sample Survey",
+  {
+    survey_dir = file.path(surveysdir, "Long Exhaustive Sample Survey")
+    qsf = file.path(survey_dir, "Long_Exhaustive_Sample_Survey.qsf")
+    csv = file.path(survey_dir, "Long_Exhaustive_Sample_Survey.csv")
+    # Use capture.output to suppress printed messages from get_setup in testing.
+    # Syntax from https://stackoverflow.com/questions/2723034/suppress-one-commands-output-in-r
+    capture.output(get_setup(
+      headerrows = 3,
+      qsf_path = qsf,
+      csv_path = csv
+    ), file = 'NUL')
 
-  index1 = find_question_index(questions,'Q2')
-})
+    index = find_question_index(questions, 'Q10')
+    df = questions[[index]][['Table']]
+    comparison_df <-
+      readRDS(
+        file.path(
+          find.package('QualtricsTools'),
+          'data/Sample Surveys/Long Exhaustive Sample Survey/RDS',
+          'Q10_results_table.rds'
+        )
+      )
+    expect_identical(df, comparison_df)
+  }
+)
